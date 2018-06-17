18c8b471b737c68d7b28adb7b61dd829df178fca
Chiranjeeb Mahanta <chiranjeebm2410@gmail.com>
2018-06-18 00:47:50 +0530

    added product info bar

--- src/Controller/AmazonTestAPIController.php
+++ src/Controller/AmazonTestAPIController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
+use Drupal\affiliates_connect\Entity\AffiliatesProduct;
 
 /**
  * Controller routines for test_api routes.
@@ -77,9 +78,7 @@ class TestAPIController extends ControllerBase {
     }
     return $results;
 
-    $response = $client->get($url, [
-      'results' => $results,
-    ]);
+    $response = $this->get($url, ['results' => $results,]);
     $contents = new SimpleXMLElement($response->getBody()->getContents());
     $json = json_encode($contents);
     $body = json_decode($json, true);
@@ -91,13 +90,80 @@ class TestAPIController extends ControllerBase {
     return $categories;
   }
 
+  /**
+   * Collect product data.
+   *
+   * @param string $item_url
+   *   The API endpoint through which the request is to be made.
+   *
+   */
+  public function products($item_url) {
+
+    $associate_id = $this->config('affiliates_connect_amazon.settings')->get('amazon_associate_id');
+    $access_key = $this->config('affiliates_connect_amazon.settings')->get('amazon_associate_id');
+    $secret_key = $this->config('affiliates_connect_amazon.settings')->get('amazon_secret_key');
+
+    $results = [];
+    foreach(array_chunk($items, 10) as $asins) {
+      $request = new AmazonRequest($this->$secret_key, $this->$access_key, $this->$associate_id);
+      $request->setOptions([
+        'Service' => 'AWSECommerceService',
+        'ItemId' => implode(',', $asins),
+        'ResponseGroup' => 'Small,Images',
+        'Operation' => 'ItemLookup',
+      ]);
+      $results = array_merge($results, $request->execute()->getResults());
+    }
+    return $results;
+
+    $response = $this->get($url, ['results' => $results,]);
+    $contents = new SimpleXMLElement($response->getBody()->getContents());
+    $json = json_encode($contents);
+    $item_data = json_decode($json, true);
+    foreach ($item_data['items'] as $key => $value) {
+      try {
+        $uid = \Drupal::currentUser()->id();
+        $product = AffiliatesProduct::create([
+          'uid' => $uid,
+          'name' => $value['itemBaseInfoV1']['title'],
+          'plugin_id' => 'affiliates_connect_amazon',
+          'product_id' => $value['itemBaseInfoV1']['itemId'],
+          'product_description' => $value['itemBaseInfoV1']['itemDescription'],
+          'product_warranty' => $value['itemBaseInfoV1']['itemWarranty'],
+          'image_urls' => $value['itemBaseInfoV1']['imageUrls']['400x400'],
+          'product_family' => $value['itemBaseInfoV1']['categoryPath'],
+          'currency' => $value['itemBaseInfoV1']['maximumRetailPrice']['currency'],
+          'maximum_retail_price' => $value['itemBaseInfoV1']['maximumRetailPrice']['price'],
+          'vendor_selling_price' => $value['itemBaseInfoV1']['amazonSellingPrice']['price'],
+          'vendor_special_price' => $value['itemBaseInfoV1']['amazonSpecialPrice']['price'],
+          'product_url' => $value['itemBaseInfoV1']['productUrl'],
+          'product_brand' => $value['itemBaseInfoV1']['productBrand'],
+          'in_stock' => $value['itemBaseInfoV1']['inStock'],
+          'cod_available' => $value['itemBaseInfoV1']['codAvailable'],
+          'discount_percentage' => $value['itemBaseInfoV1']['discountPercentage'],
+          'offers' => implode(',', $value['itemBaseInfoV1']['offers']),
+          'size' => $value['itemBaseInfoV1']['attributes']['size'],
+          'color' => $value['itemBaseInfoV1']['attributes']['color'],
+          'seller_name' => $value['itemShippingInfoV1']['sellerName'],
+          'seller_average_rating' => $value['itemShippingInfoV1']['sellerAverageRating'],
+          'additional_data' => '',
+          'status' => 1,
+        ]);
+        $product->save();
+      }
+      catch (Exception $e) {
+        echo $e->getMessage();
+      }
+    }
+
+  }
 
 #  /**
 #   * Callback for `affiliates_connect_amazon/post.json` API method.
 #   */
 #  public function post_example( Request $request ) {
-#    // This condition checks the `Content-type` and makes sure to
 #    // decode JSON string from the request body into array.
+#    // This condition checks the `Content-type` and makes sure to
 #    if ( 0 === strpos( $request->headers->get( 'Content-Type' ), 'application/json' ) ) {
 #      $data = json_decode( $request->getContent(), TRUE );
 #      $request->request->replace( is_array( $data ) ? $data : [] );
